db3de1313a683213297e50a69255cb4c94f42702
Jesús Rodrigo <jesus.rodrigo@gmail.com>
2015-12-23 16:54:27 +0100

    Fix Gambit-C directory and remove marmalade package repo

--- init/envvars.el
+++ init/envvars.el
@@ -2,7 +2,7 @@
 (add-to-list 'Info-default-directory-list "/home/jesus/local/sbcl/share/info/")
 (add-to-list 'Info-default-directory-list "/home/jesus/local/mit-scheme/share/info/")
 ;; (add-to-list 'Info-default-directory-list "/home/jesus/local/guile/share/info/")
-(add-to-list 'Info-default-directory-list "/home/jesus/local/gambit/current/info/")
+(add-to-list 'Info-default-directory-list "/home/jesus/local/Gambit-C/current/info/")
 (add-to-list 'Info-default-directory-list "/home/jesus/local/vicare/share/info/")
 
 (setq make-backup-files nil)
