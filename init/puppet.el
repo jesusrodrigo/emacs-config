e1de0e1f24ddd93b517416fc1763e5a47849e2b6
Jesús Rodrigo <jesus.rodrigo@gmail.com>
2016-02-29 23:48:14 +0000

    Alternative puppet-mode

--- init/puppet.el
+++ init/puppet.el
@@ -1,4 +1,8 @@
-(add-hook 'puppet-mode-hook
-          (lambda ()
-            (setq show-trailing-whitespace t)
-            (autopair-mode 1)))
+(eval-after-load 'puppet-mode
+  (add-hook 'puppet-mode-hook
+            (lambda ()
+              (setq show-trailing-whitespace t)
+              (autopair-mode 1))))
+(autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
+
+(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
