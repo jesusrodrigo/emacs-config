b4c4f071103612f9b26363ea6f7f277bbae58582
Jesús Rodrigo <jesus.rodrigo@gmail.com>
2016-03-14 23:40:23 +0000

    Make Emacs frame use gnome dark theme

--- init/frames.el
+++ init/frames.el
@@ -7,3 +7,15 @@
                             (tool-bar-lines . 0)))
 
 (scroll-bar-mode -1)
+(menu-bar-mode -1)
+(require 'frame-fns)
+(defun set-selected-frame-dark ()
+  (interactive)
+  (let ((frame-name (get-frame-name (selected-frame))))
+    (call-process-shell-command (concat
+                                 "xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT \"dark\" -name \""
+                                 frame-name
+                                 "\""))))
+
+(if (window-system)
+    (set-selected-frame-dark))
