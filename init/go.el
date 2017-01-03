9bf50ba54575b6c17f825427b773aa0fa65afabf
Jesús Rodrigo <jesus.rodrigo@gmail.com>
2017-01-03 06:51:10 +0000

    Switch to go-guru instead of godef

--- init/go.el
+++ init/go.el
@@ -1,5 +1,5 @@
 (require 'go-mode)
-
+(require 'go-guru)
 (require 'go-eldoc)
 (require 'company)
 (require 'company-go)
@@ -8,8 +8,6 @@
 (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
 (setq company-echo-delay 0)                          ; remove annoying blinking
 (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
-
-(load "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
 
 ;; Hooks
 (add-hook 'go-mode-hook 'my-go-hook)
@@ -36,14 +34,16 @@
   ;; Company
   (set (make-local-variable 'company-backends) '(company-go))
   (company-mode 1)
-  
+
+  ;; Guru
+  (local-set-key (kbd "M-.") 'go-guru-definition)
+  (go-guru-hl-identifier-mode 1)
   ;; Other
   (autopair-mode 1)
   (go-eldoc-setup)
   (setq tab-width 8)
   (setq show-trailing-whitespace t)
-  (setq gofmt-command "goimports")
-  (local-set-key (kbd "M-.") 'godef-jump))
+  (setq gofmt-command "goimports"))
 
 ;; helper function
 (defun go ()
