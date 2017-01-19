d126b58011512086b49b53a8aa4dfc2ee40a6c10
Jesús Rodrigo <jesus.rodrigo@gmail.com>
2017-01-19 19:07:55 +0000

    Rust mode tweaks

--- init/rust.el
+++ init/rust.el
@@ -3,23 +3,24 @@
 
 (defun my-rust-hook ()
   ;; Teclas
-  (define-key rust-mode-map "\r" 'newline-and-indent)
-  (define-key rust-mode-map "\n" 'newline)
+  ;; (define-key rust-mode-map "\r" 'newline-and-indent)
+  ;; (define-key rust-mode-map "\n" 'newline)
   (racer-mode 1)
   (cargo-minor-mode 1)
   (autopair-mode 1)
-  (setq indent-tabs-mode nil))
+  ;; (setq indent-tabs-mode nil)
+  )
 
 (add-hook 'rust-mode-hook 'my-rust-hook)
 
-;; Racer
+;;; Racer
 (setq racer-cmd "~/.cargo/bin/racer")
-(setq racer-rust-src-path "/home/jesus/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src")
+;; (setq racer-rust-src-path "/home/jesus/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src")
 
 (defun my-racer-hook ()
   (define-key racer-mode-map (kbd "TAB") #'company-indent-or-complete-common)
   (define-key racer-mode-map (kbd "C-c C-d") #'racer-describe)
-  (auto-composition-mode 1)
+  ;; (auto-composition-mode 1)
   (setq company-tooltip-align-annotations t)
   (company-mode 1)
   (eldoc-mode 1))
