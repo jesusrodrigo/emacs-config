3e3a547c2fd6dacece7b05adc3663526fd833f82
Jesús Rodrigo <jrodrigo@salesforce.com>
2017-02-09 01:02:13 +0000

    Don't use frame-fns on macOS

--- init/frames.el
+++ init/frames.el
@@ -7,18 +7,18 @@
                             (tool-bar-lines . 0)))
 
 (scroll-bar-mode -1)
-;; (menu-bar-mode 1)
-(require 'frame-fns)
-(defun set-selected-frame-dark ()
-  (interactive)
-  (let ((frame-name (get-frame-name (selected-frame))))
-    (call-process-shell-command (concat
-                                 "xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT \"dark\" -name \""
-                                 frame-name
-                                 "\""))))
-
-;; (if (window-system)
-;;     (set-selected-frame-dark))
+(menu-bar-mode 1)
+(when (eq window-system 'x)
+  (require 'frame-fns)
+  (defun set-selected-frame-dark ()
+    (interactive)
+    (let ((frame-name (get-frame-name (selected-frame))))
+      (call-process-shell-command (concat
+                                   "xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT \"dark\" -name \""
+                                   frame-name
+                                   "\""))))
+  (if (window-system)
+      (set-selected-frame-dark)))
 
 (add-to-list 'default-frame-alist '(font . "-*-Source Code Pro-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
 (set-face-attribute 'default t :font "-*-Source Code Pro-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
