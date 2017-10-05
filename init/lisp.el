cf49ebf9f847041c567a75b4847b9dbd9ae7074a
Jesús Rodrigo <jesus.rodrigo@gmail.com>
2017-10-05 23:44:04 +0100

    Use sly instead of slime

--- init/lisp.el
+++ init/lisp.el
@@ -1,50 +1,43 @@
 ;;; Common-Lisp
-(load (expand-file-name "~/local/quicklisp/slime-helper.el"))
+(require 'sly-autoloads)
 (setq inferior-lisp-program "sbcl")
 
-;; Slime
-(require 'slime)
+;; ;; Slime
+;; (require 'slime)
 
-(setq slime-lisp-implementations
+(setq sly-lisp-implementations
       '((sbcl ("/home/jesus/local/sbcl/bin/sbcl") :coding-system utf-8-unix)
         (ccl ("ccl") :coding-system utf-8-unix)))
 
-(defun ccl () (interactive) (slime 'ccl))
-(defun sbcl () (interactive) (slime 'sbcl))
+(defun ccl () (interactive) (sly 'ccl))
+(defun sbcl () (interactive) (sly 'sbcl))
 
-;; slime-contrib
-;; (slime-setup '(slime-banner
-;;                slime-asdf
-;;                slime-fancy
-;;                slime-indentation
-;;                slime-compiler-notes-tree))
-(slime-setup '(slime-fancy
-               slime-asdf))
+(sly-setup '(sly-fancy))
 
-(setq slime-complete-symbol*-fancy t)
-(setq slime-complete-symbol-function 'slime-complete-symbol*)
-;;(setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
-(setq slime-startup-animation nil)
-(setq slime-autodoc-use-multiline-p t)
-;;(setq slime-use-autodoc-mode nil)
-;;  (setq slime-enable-evaluate-in-emacs t)
+;; (setq slime-complete-symbol*-fancy t)
+;; (setq slime-complete-symbol-function 'slime-complete-symbol*)
+;; ;;(setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
+;; (setq slime-startup-animation nil)
+;; (setq slime-autodoc-use-multiline-p t)
+;; ;;(setq slime-use-autodoc-mode nil)
+;; ;;  (setq slime-enable-evaluate-in-emacs t)
 
 ;; ;; (setq common-lisp-hyperspec-root
 ;; ;;       (concat "file:" (expand-file-name "~/local/share/doc/HyperSpec/")))
 
-(setq auto-mode-alist
-      (append '(("\\.lsp$" . lisp-mode)
-                ("\\.cl$" . lisp-mode)
-                ("\\.asd$" . lisp-mode)
-                ("\\.system$" . lisp-mode))
-              auto-mode-alist))
+;; (setq auto-mode-alist
+;;       (append '(("\\.lsp$" . lisp-mode)
+;;                 ("\\.cl$" . lisp-mode)
+;;                 ("\\.asd$" . lisp-mode)
+;;                 ("\\.system$" . lisp-mode))
+;;               auto-mode-alist))
 
-(add-hook 'lisp-mode-hook
-          (lambda ()
-            (setq tab-always-indent 'complete)
-            (define-key slime-mode-map (kbd "TAB") 'slime-indent-and-complete-symbol)
-            (paredit-mode 1)))
+;; (add-hook 'lisp-mode-hook
+;;           (lambda ()
+;;             (setq tab-always-indent 'complete)
+;;             (define-key slime-mode-map (kbd "TAB") 'slime-indent-and-complete-symbol)
+;;             (paredit-mode 1)))
 
-(global-set-key "\C-cs" 'slime-selector)
-(global-set-key [f2] 'slime-selector)
-(global-set-key [f3] 'slime-hyperspec-lookup)
+(global-set-key "\C-cs" sly-selector-map)
+;; (global-set-key [f2] 'slime-selector)
+;; (global-set-key [f3] 'slime-hyperspec-lookup)
